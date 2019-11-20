311aafb3d45d94612175bda2b12f9f9d944fff91
Gabriel Conrado <gabriel.conrado@loggi.com>
2019-11-20 03:09:24 -0300

    Implemented user add item FT

---
+++ tests/functional/test_marketlist.py
@@ -0,0 +1,26 @@
+'''
+@file:     test_marketlist.py
+@author:   Gaba <gabaconrado@gmail.com>
+@brief:    Functional tests for the marketlist module
+@date:     2019
+'''
+
+import pytest
+from selenium import webdriver
+
+
+@pytest.fixture
+def browser():
+    _browser = webdriver.Firefox()
+    _browser.get('http://localhost:8000')
+    yield _browser
+    _browser.quit()
+
+
+def test_user_can_add_item(browser):
+    # Letício enter the website and notice a "Market list" blank page with a textfield and a button
+    assert browser.title == 'Peculiar fox'
+    assert browser.find_elements_by_id('div_header').text == 'Market list'
+    assert not browser.find_elements_by_id('div_list')
+    # Letício type an item and presses the button, the added item is shown and the field is cleared
+    assert 1 == 2
