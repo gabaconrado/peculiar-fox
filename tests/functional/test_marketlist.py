c3766504729503628801165639c553f0e4eaa863
Gabriel Conrado <gabriel.conrado@loggi.com>
2019-11-20 13:21:10 -0300

    Refactored functional test to use less find_elements...

--- tests/functional/test_marketlist.py
+++ tests/functional/test_marketlist.py
@@ -7,6 +7,11 @@
 
 import pytest
 from selenium import webdriver
+#from selenium.webdriver.common.by import By
+#from selenium.webdriver.support.ui import WebDriverWait
+#from selenium.webdriver.support import expected_conditions as EC
+
+MAX_TIMEOUT = 5
 
 
 @pytest.fixture
@@ -20,10 +25,17 @@ def browser():
 def test_user_can_add_item(browser):
     # Letício enter the website and notice a "Market list" blank page with a textfield and a button
     # The textfield and the list are clear
+    content_title = browser.find_element_by_id('content_title')
+    item_input = browser.find_element_by_id('txt_item')
+    btn_submit = browser.find_element_by_id('btn_submit')
+    table_list = browser.find_element_by_id('table_list')
+    rows_list = table_list.find_elements_by_tag_name('tr')
     assert browser.title == 'Peculiar fox'
-    assert browser.find_element_by_id('div_header').text == 'Market list'
-    assert not browser.find_element_by_id('txt_item').text
-    assert browser.find_element_by_id('btn_submit')
-    assert not browser.find_element_by_id('div_list').text
+    assert content_title.text == 'Market list'
+    assert not item_input.text
+    assert btn_submit
+    assert table_list
+    assert len(rows_list) == 0
     # Letício type an item and presses the button, the added item is shown and the field is cleared
-    assert 1 == 2
+#    browser.find_element_by_id('txt_item').sendKeys('Milk')
+#    browser.find_element_by_id('btn_submit').click()
