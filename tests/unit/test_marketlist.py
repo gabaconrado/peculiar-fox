ef492142f07e4cb83316ca3b6d1fb6e1365f83b6
Gabriel Conrado <gabriel.conrado@loggi.com>
2019-11-20 16:35:22 -0300

    refactor(tests): improved fixtures

--- tests/unit/test_marketlist.py
+++ tests/unit/test_marketlist.py
@@ -5,44 +5,46 @@
 @date:     2019
 '''
 import pytest
-from django.test import Client
 from django.urls import reverse
 from marketlist import views
 from marketlist.models import Item
 
+METHOD_GET = 0
+METHOD_POST = 1
+
 
 @pytest.fixture
-def client():
-    return Client()
+def item(db):
+    return Item.objects.create(name='Milk')
 
 
-def test_list_route(client, db):
-    path = reverse('marketlist:marketlist')
-    response = client.get(path)
+@pytest.fixture
+def response(db, client, path, method, data):
+    if method == METHOD_GET:
+        return client.get(reverse(path))
+    return client.post(reverse(path), data)
+
+
+@pytest.mark.parametrize('path,method,data', [('marketlist:marketlist', METHOD_GET, None)])
+def test_list_route(response):
     assert response.status_code == 200
     assert response.resolver_match.func == views.marketlist
     assert response.templates[0].name == 'marketlist.html'
 
 
-def test_home_redir_to_marketlist(client, db):
-    path = reverse('marketlist:home')
-    response = client.get(path)
+@pytest.mark.parametrize('path,method,data', [('marketlist:home', METHOD_GET, None)])
+def test_home_redir_to_marketlist(response):
     assert response.status_code == 302
     assert response.get('location') == reverse('marketlist:marketlist')
 
-
-def test_add_new_item_and_redirect(client, db):
-    path = reverse('marketlist:marketlist')
-    data = {'name': 'Milk'}
-    response = client.post(path, data)
+@pytest.mark.parametrize(
+    'path,method,data', [('marketlist:marketlist', METHOD_POST, {'name': 'Milk'})])
+def test_add_new_item_and_redirect(response):
     assert Item.objects.count() == 1, 'Object not inserted'
     assert Item.objects.first().name == 'Milk', 'Wrong object inserted'
     assert response.status_code == 302
     assert response.get('location') == reverse('marketlist:marketlist')
 
-
-def test_add_new_item_and_show(client, db):
-    new_item = Item.objects.create(name='Milk')
-    path = reverse('marketlist:marketlist')
-    response = client.get(path)
-    assert new_item.name in response.content.decode()
+@pytest.mark.parametrize('path,method,data', [('marketlist:marketlist', METHOD_GET, None)])
+def test_add_new_item_and_show(item, response):
+    assert item.name in response.content.decode()
