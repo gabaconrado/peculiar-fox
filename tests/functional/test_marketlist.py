7efa7ad67555e8dab2199e7012d0bf6f4b15ec6e
Gabriel Conrado <gabriel.conrado@loggi.com>
2019-11-20 17:18:09 -0300

    refactor(tests): Removed sleeps and improved insertion test

--- tests/functional/test_marketlist.py
+++ tests/functional/test_marketlist.py
@@ -9,9 +9,12 @@ import pytest
 from time import sleep
 from selenium import webdriver
 from selenium.webdriver.common.keys import Keys
+from selenium.webdriver.common.by import By
+from selenium.webdriver.support.ui import WebDriverWait
+from selenium.webdriver.support import expected_conditions as EC
 
-MAX_TIMEOUT = 5
 
+MAX_TIMEOUT = 3
 BROWSER_TITLE = 'Peculiar fox'
 PAGE_TITLE = 'Market list'
 TITLE_ID = 'content_title'
@@ -38,19 +41,17 @@ def test_user_can_add_item(browser):
     assert browser.title == BROWSER_TITLE
     assert browser.find_element_by_id(TITLE_ID).text == PAGE_TITLE
     assert not browser.find_element_by_id(INPUT_ID).text
-    assert len(browser.find_element_by_id(TABLE_ID).find_elements_by_tag_name('tr')) == 0
+    assert not len(browser.find_element_by_id(TABLE_ID).find_elements_by_tag_name('td'))
     # Letício type an item and presses the button, the added item is shown and the field is cleared
     browser.find_element_by_id(INPUT_ID).send_keys(ITEM_NAME_A)
     browser.find_element_by_id(BTN_SUBMIT_ID).click()
-    sleep(1)
-    assert \
-        len(browser.find_element_by_id(TABLE_ID).find_elements_by_tag_name('tr')) == 1, \
-        'Element was not added to the list'
+    WebDriverWait(browser, MAX_TIMEOUT).until(
+        EC.text_to_be_present_in_element((By.ID, 'item_1'), ITEM_NAME_A)
+    )
     # Letício then tries to add a new item using enter instead of clicking the button
     # He sees that it works and the new item is also added to the list
     browser.find_element_by_id(INPUT_ID).send_keys(ITEM_NAME_B)
     browser.find_element_by_id(INPUT_ID).send_keys(Keys.ENTER)
-    sleep(1)
-    assert \
-        len(browser.find_element_by_id(TABLE_ID).find_elements_by_tag_name('tr')) == 2, \
-        'New element was not added to the list'
+    WebDriverWait(browser, MAX_TIMEOUT).until(
+        EC.text_to_be_present_in_element((By.ID, 'item_2'), ITEM_NAME_B)
+    )
