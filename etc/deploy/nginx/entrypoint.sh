fa72a7dbe1720003b7c5f630ac6c5eca540f5e71
Gabriel Conrado <gabriel.conrado@loggi.com>
2019-11-28 14:32:32 -0300

    chode(project): Change nginx service workdir

--- etc/deploy/nginx/entrypoint.sh
+++ etc/deploy/nginx/entrypoint.sh
@@ -8,6 +8,6 @@ then
 fi
 
 # Replace the values in the nginx template for the environment vars
-envsubst < /etc/nginx/conf.d/peculiar-fox.conf > /etc/nginx/conf.d/peculiar-fox.conf
+envsubst < /etc/server/peculiar-fox.conf > /etc/nginx/conf.d/peculiar-fox.conf
 
 exec "$@"
